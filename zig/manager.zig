bc8f1fd82badb7a4557c7c37c292eb5093fd49cb
ItsMeSamey <sameychain5041@gmail.com>
2024-06-04 11:17:27 +0530

    manager fix

--- zig/manager.zig
+++ zig/manager.zig
@@ -33,19 +33,28 @@ inline fn getAuth(udir: fs.Dir, password: [:0]const u8, allocator: std.mem.Alloc
     return error.@"getAuth readToEndAlloc error";
   };
   _ = std.mem.indexOfDiff(u8, password, file_pass) orelse {
-    try stdout.print("Auth Success", .{});
+    try stdout.print("Auth Success\n", .{});
   };
-  try stdout.print("Invalid Password", .{});
+  try stdout.print("Invalid Password\n", .{});
 }
 
 inline fn createUser(db: fs.Dir, username: [:0]const u8, password: [:0]const u8) !void {
+  if (@TypeOf(db.openDir(username, .{})) == @TypeOf(std.fs.Dir)) return error.@"user already present";
   db.makeDir(username) catch return error.@"createUser makedir error";
   var udir = db.openDir(username, .{}) catch return error.@"createUser openDir error";
   defer udir.close();
   var file = udir.createFile(".password", .{}) catch return error.@"createUser createFile error";
   defer file.close();
   _ = file.write(password) catch return error.@"createUser write error";
-  try stdout.print("User Created");
+  try stdout.print("User Created\n", .{});
+}
+
+inline fn changePass(db: fs.Dir, username: [:0]const u8, password: [:0]const u8) !void {
+  const udir = getDir(db, username) orelse return error.@"changePass getDir error";
+  var file = udir.createFile(".password", .{}) catch return error.@"createUser createFile error";
+  defer file.close();
+  _ = file.write(password) catch return error.@"createUser write error";
+  try stdout.print("Password Changed\n", .{});
 }
 
 inline fn deleteUser(db: fs.Dir, username: [:0]const u8) !void {
@@ -54,9 +63,10 @@ inline fn deleteUser(db: fs.Dir, username: [:0]const u8) !void {
 
 pub fn main() !void{
   var buffer: [4 << 10]u8 = undefined;
+  var Allocator = std.heap.FixedBufferAllocator.init(&buffer);
+  const allocator = Allocator.allocator();
+
   const DB = try fs.cwd().openDir("db", .{});
-  var allocator = std.heap.FixedBufferAllocator.init(&buffer).allocator();
-  _ = &allocator;
   var args = std.process.args();
   _ = args.skip();
 
@@ -65,14 +75,15 @@ pub fn main() !void{
     return error.@"no username";
   };
   if (!(try isValidUsername(username))) return stdout.print("Invalid Username\n", .{});
-
-  const udir = getDir(DB, username) orelse return stdout.print("User Does Not Exist\n", .{});
+  
   const password = args.next() orelse return stdout.print("User Exists\n", .{});
-  const command = args.next() orelse return getAuth(udir, password, allocator);
+  const command = args.next() orelse {
+    const udir = getDir(DB, username) orelse return stdout.print("User Does Not Exist\n", .{});
+    return getAuth(udir, password, allocator);
+  };
 
   if (std.mem.eql(u8, command, "create")) return createUser(DB, username, password);
-
-  // return .{username, password};
-  // try stderr.print("{s}\n{s}\n", .{x.@"0", x.@"1"});
+  if (std.mem.eql(u8, command, "change")) return changePass(DB, username, password);
+  if (std.mem.eql(u8, command, "delete")) return deleteUser(DB, username);
 }
 
